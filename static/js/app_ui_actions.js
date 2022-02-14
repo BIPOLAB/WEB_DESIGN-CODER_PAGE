b2d9b67d21569890966aeab7dcf8d69bd1171ab0
BIPOLAB engineering <lcontarino@bipolab.com.ar>
2022-02-14 00:20:46 -0300

    import

--- static/js/app_ui_actions.js
+++ static/js/app_ui_actions.js
@@ -1,8 +1,3 @@
-var pad_btn_01 = document.getElementById('pad_01')
-pad_btn_01.addEventListener('click', function () {
-    pad_btn_01.style.backgroundColor = 'rgb(225, 225, 214)';
-});
-
 const drop__menu = document.querySelector(".drop__menu"),
     drop__modeBtn = drop__menu.querySelector(".drop__modeBtn");
     drop__modeBtn.addEventListener("click", () => {
@@ -19,3 +14,4 @@ const note__dropMenu = document.querySelector(".note__dropMenu"),
     drop__noteBtn.addEventListener("click", () => {
         note__dropMenu.classList.toggle("toggle__dropNote");
 });
+
